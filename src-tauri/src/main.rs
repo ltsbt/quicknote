f53df91ce972462586e8a1a02b019f31f80d116c
ltsbt <82132888+ltsbt@users.noreply.github.com>
2023-12-27 03:20:37 +0000

    Make app system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,73 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
+
+#[tauri::command]
+fn create_note() {
+    println!("Creating note");
+}
 
 fn main() {
+    let new_note = CustomMenuItem::new("new_note", "New Note");
+    let settings = CustomMenuItem::new("settings", "Settings");
+    let about = CustomMenuItem::new("about", "About");
+    let quit = CustomMenuItem::new("quit", "Quit");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(new_note)
+        .add_item(settings)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(about)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            tauri::WindowEvent::Focused(focused) => {
+                if !*focused {
+                    event.window().hide().unwrap();
+                }
+            }
+            _ => {}
+        })
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+                window.set_focus().unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "new_note" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+                "settings" => {
+                    let window = app.get_window("settings").unwrap();
+                    window.hide().unwrap();
+                }
+                "about" => {}
+                "quit" => {
+                    app.exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![create_note])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while building tauri application");
 }
---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { invoke } from '@tauri-apps/api/tauri';
+
+    let text = '';
+
+    function createNote() {
+        invoke('create_note', { text });
+        text = '';
+    }
+</script>
+
+<main class="overflow-x-hidden flex flex-col items-center justify-center">
+    <h1 class="text-4xl m-8">New Note:</h1>
+    <textarea bind:value={text} class="block w-full h-full p-2" placeholder="Note content" />
+    <button on:click={createNote} class="font-bold py-2 px-4 rounded">Create Note</button>
+</main>
